cf3ffe1bec6641edf2fb21053b860527b1e7743d
ChayaninNapia <ChayaninNapia@gmail.com>
2023-11-05 18:40:06 +0700

    V1.5(complete number )

--- ElecLab01/Core/Src/main.c
+++ ElecLab01/Core/Src/main.c
@@ -68,6 +68,7 @@ uint16_t ButtonState = 0;
 uint16_t num = 0;
 uint16_t myArray[11] = {0,0,0,0,0,0,0,0,0};
 uint16_t keys[11] = {7,4,1,0,8,5,2,9,9,6,3};
+uint16_t password[11] = {6,5,3,4,0,5,0,0,0,0,9};
 int top = -1;
 int status = 0;
 
@@ -85,6 +86,8 @@ void ButtonMatrixRead();
 /* USER CODE BEGIN 0 */
 void setArr(uint16_t state){
 	int realnum = log2(state);
+	if(realnum == 7 || realnum == 11)
+		return;
 	top++;
 	myArray[top] = keys[realnum];
 }
@@ -144,8 +147,20 @@ int main(void)
 		{
 			ButtonMatrixRead();
 		}
+	  }
+		else if(status == 0){
+			while(status == 0){
+				GPIO_PinState S = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10);
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, S);
+				status = S;
+			}
+			top = -1;
+			  for (int i = 0; i < 11; i++) {
+					myArray[i] = 0;
+				}
+		}
 
-	}
+
 
 
 	  }
